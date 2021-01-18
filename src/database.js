52aad47655650eddbd54e5c9fc2446da5c3201e6
ChanakaUOMIT <chanakauomfit@gmail.com>
2021-01-19 00:38:51 +0530

    Updated MongoDB Connection String

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import config from "./config.js";
 
-const MONGODB_URI = `mongodb://${config.MONGODB_HOST}/${config.MONGODB_DATABASE}`;
+const MONGODB_URI = `mongodb+srv://${config.MONGODB_HOST}/${config.MONGODB_DATABASE}`;
 
 mongoose
     .connect(MONGODB_URI, {
